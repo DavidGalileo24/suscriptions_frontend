fd8361b13e2d36f55e6bf24865c178be8ba19395
DavidGalileo24 <david.salgado.mejia@gmail.com>
2023-11-13 16:52:30 -0600

    Config components call in main file :sparkles:

--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+const components = import.meta.globEager('./components/*.vue');
 const app = createApp(App)
+const pinia = createPinia() 
+pinia.use(( {store} ) => {
+  store.router = markRaw(router)
+})
 
-app.use(createPinia())
 app.use(router)
-
+app.use(pinia)
+Object.entries(components).forEach(([path, component]) => {
+    app.component(path.split('/').pop().replace(/\.\w+$/, ''), component.default);
+  });
 app.mount('#app')
